ce0237952d3ec0a41683d4a9ab9eb66e047436f0
Glynnis Thomas <glynnisthomas@gmail.com>
2018-03-05 23:22:11 -0600

    Moved ViewModels / Added new ViewModels

---
+++ MedManager/ViewModels/AddMedViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MedManager.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MedManager.ViewModels
+{
+    public class AddMedViewModel
+    {
+        [Required]
+        [Display(Name = "Medication Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Dosage")]
+        public int Dosage { get; set; }
+
+        // public int PillsPerDose { get; set; }
+
+        [Required]
+        [Display(Name = "Number of Times Taken Per Day")]
+        public int TimesXDay { get; set; }
+
+        public string Notes { get; set; }
+
+        public int RefillRate { get; set; }
+
+
+        public AddMedViewModel() : base() { }
+
+        /*
+        public AddMedViewModel(IEnumerable<ToD> times)
+        {
+            ToDay = new List<SelectListItem>();
+            foreach(ToD time in times)
+            {
+                ToDay.Add(new SelectListItem
+                {
+                    Value = time.ToString(),
+                    Text = time.ToString()
+                });
+            }
+        }*/
+    }
+}
---
+++ MedManager/ViewModels/EditMedViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using MedManager.Models;
+
+namespace MedManager.ViewModels
+{
+    public class EditMedViewModel
+    {
+        [Required]
+        public int MedId { get; set; }
+
+        public Medication Med { get; set; }
+        /*
+        public int ScripNumber { get; set; }
+        public int PillsPerDose { get; set; }
+        public string PrescribingDoctor { get; set; }
+        public string Pharmacy { get; set; }
+        
+        public List<SelectListItem> Times { get; set; }
+        public ToD SelectedTime { get; set; }
+        */
+        public EditMedViewModel() : base() { }
+
+        public EditMedViewModel(Medication med)
+        {
+            Med = med;
+            /* for other var: , IEnumerable<ToD> times
+            Times = new List<SelectListItem>();
+            foreach (ToD time in times)
+            {
+                Times.Add(new SelectListItem
+                {
+                    Value = time.ToString(),
+                    Text = time.ToString()
+                });
+            } */
+        }
+    }
+}
