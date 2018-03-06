d39a1559270ed17d312e25daea5001660296402f
Glynnis Thomas <glynnisthomas@gmail.com>
2018-03-05 20:41:26 -0600

    Add project files.

---
+++ MedManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MedManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
