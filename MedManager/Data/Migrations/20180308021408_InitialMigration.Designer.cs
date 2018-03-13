5c22d0f905bf8d3781845e6f31378a41b0a4b8e9
Glynnis Thomas <glynnisthomas@gmail.com>
2018-03-13 01:02:21 -0500

    Migration

--- MedManager/Data/Migrations/20180308021408_InitialMigration.Designer.cs
+++ MedManager/Data/Migrations/20180308021408_InitialMigration.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace MedManager.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180308021408_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MedManager/Data/Migrations/20180308021408_InitialMigration.cs
@@ -0,0 +1,228 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MedManager.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "permissionLevel",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Medication",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ApplicationUserId = table.Column<string>(nullable: true),
+                    Dosage = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Notes = table.Column<string>(nullable: true),
+                    RefillRate = table.Column<int>(nullable: false),
+                    TimesXDay = table.Column<int>(nullable: false),
+                    UserID = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medication", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Medication_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Set",
+                columns: table => new
+                {
+                    SetID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    TimeOfSet = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Set", x => x.SetID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserMeds",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false),
+                    MedID = table.Column<int>(nullable: false),
+                    MedicationID = table.Column<int>(nullable: true),
+                    UserId1 = table.Column<string>(nullable: true),
+                    UserMedID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserMeds", x => new { x.UserId, x.MedID });
+                    table.ForeignKey(
+                        name: "FK_UserMeds_Medication_MedicationID",
+                        column: x => x.MedicationID,
+                        principalTable: "Medication",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserMeds_AspNetUsers_UserId1",
+                        column: x => x.UserId1,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Doses",
+                columns: table => new
+                {
+                    DoseID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    PillsXDose = table.Column<int>(nullable: false),
+                    SetID = table.Column<int>(nullable: true),
+                    TimeOfDose = table.Column<int>(nullable: false),
+                    UserMedMedID = table.Column<int>(nullable: false),
+                    UserMedUserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doses", x => x.DoseID);
+                    table.ForeignKey(
+                        name: "FK_Doses_Set_SetID",
+                        column: x => x.SetID,
+                        principalTable: "Set",
+                        principalColumn: "SetID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Doses_UserMeds_UserMedUserId_UserMedMedID",
+                        columns: x => new { x.UserMedUserId, x.UserMedMedID },
+                        principalTable: "UserMeds",
+                        principalColumns: new[] { "UserId", "MedID" },
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doses_SetID",
+                table: "Doses",
+                column: "SetID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doses_UserMedUserId_UserMedMedID",
+                table: "Doses",
+                columns: new[] { "UserMedUserId", "UserMedMedID" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medication_ApplicationUserId",
+                table: "Medication",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserMeds_MedicationID",
+                table: "UserMeds",
+                column: "MedicationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserMeds_UserId1",
+                table: "UserMeds",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "AspNetUserTokens",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "Doses");
+
+            migrationBuilder.DropTable(
+                name: "Set");
+
+            migrationBuilder.DropTable(
+                name: "UserMeds");
+
+            migrationBuilder.DropTable(
+                name: "Medication");
+
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "permissionLevel",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName");
+        }
+    }
+}
